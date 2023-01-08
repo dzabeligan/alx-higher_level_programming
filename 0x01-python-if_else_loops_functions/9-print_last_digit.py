be1ba724b10e6cdf8cde7c3a6e60414b4885d188
Elijah Balogun <elijah.balogun@iisysgroup.com>
2023-01-08 18:09:50 +0100

    feat: Print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    """Print the last digit of a number and return it."""
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return (last_digit)
