1f97af5a17b7f0a857a1e0edae376134280bd577
Elijah Balogun <elijah.balogun@iisysgroup.com>
2023-01-19 14:02:34 +0100

    feat: Implement new in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Task 4
+    """
+
+
+def new_in_list(my_list, idx, element):
+    """new_in_list: replace item in list. Original list is not mutated
+
+    Args:
+        my_list (Any): list
+        idx (number): index
+        element (Any): list item
+
+    Returns:
+        Any: list copy
+    """
+    copy = my_list.copy()
+
+    if idx >= 0 and idx < len(my_list):
+        copy[idx] = element
+    return copy
