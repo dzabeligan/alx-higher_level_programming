564c367ca92abacc00e14560f220ec85fe5eceb3
Elijah Balogun <elijah.balogun@iisysgroup.com>
2023-01-20 12:03:44 +0100

    feat: Implement `get number of keys in dictionary`

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Task 5"""
+
+
+def number_keys(a_dictionary):
+    """get number of keys in dictionary
+
+    Args:
+        a_dictionary (dictionary): dictionary
+
+    Returns:
+        number: number of keys in dictionary
+    """
+    return len(a_dictionary)
