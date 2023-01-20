7c15ea51abca67d1aa72f5f7997e4899ca8364cc
Elijah Balogun <elijah.balogun@iisysgroup.com>
2023-01-20 12:24:50 +0100

    feat: Implement update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Task 7"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Update dictionary
+
+    Args:
+        a_dictionary (dictionary): dictionary
+        key: key
+        value (Any): value
+
+    Returns:
+        dictionary: updated dictionary
+    """
+    a_dictionary[key] = value
+    return a_dictionary
