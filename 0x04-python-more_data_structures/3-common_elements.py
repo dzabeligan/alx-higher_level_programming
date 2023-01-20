d035deacfe51c26a280dd6f84889db5f9a744015
Elijah Balogun <elijah.balogun@iisysgroup.com>
2023-01-20 11:38:50 +0100

    feat: Implement intersect of two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Task 3"""
+
+
+def common_elements(set_1, set_2):
+    """perform intersect operation on two sets
+
+    Args:
+        set_1 (set): first set
+        set_2 (set): second set
+
+    Returns:
+        set: intersect of first and second set
+    """
+    return set_1 & set_2
