171fdc5263f34f0e0101cffbdc91fe1df3b712f1
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-07-10 10:34:50 +0100

    feat: Implement text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a text indentation function."""
+
+
+def text_indentation(text):
+    """
+    Prints the text with 2 new lines after each occurrence of '.', '?', and ':'.
+    There should be no space at the beginning or at the end of each printed line.
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    count = 0
+    while count < len(text) and text[count] == ' ':
+        count += 1
+
+    while count < len(text):
+        print(text[count], end="")
+        if text[count] == "\n" or text[count] in ".?:":
+            if text[count] in ".?:":
+                print("\n")
+            count += 1
+            while count < len(text) and text[count] == ' ':
+                count += 1
+            continue
+        count += 1
