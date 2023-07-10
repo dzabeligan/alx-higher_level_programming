a90fa2025688f0fc9a76d98f5dff7d7b42b8fe21
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-07-10 08:27:57 +0100

    fix: Fix tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -18,5 +18,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    full_name = first_name + " " + last_name if last_name else first_name
-    print("My name is", full_name)
+    print(f"My name is {first_name} {last_name}")
