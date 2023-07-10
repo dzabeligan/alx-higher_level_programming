35dda42d5d9b1d3f2a7c673033868495fe66929b
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-07-10 07:56:58 +0100

    feat: Implement say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines an say my name function."""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first name> <last name>".
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to an empty string.
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    full_name = first_name + " " + last_name if last_name else first_name
+    print("My name is", full_name)
