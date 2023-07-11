cfea9e3ac487f3abe42a0077785fe7892f39357f
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-07-11 05:48:39 +0100

    fix: Fix bug in validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,7 +19,7 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
-        if isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
