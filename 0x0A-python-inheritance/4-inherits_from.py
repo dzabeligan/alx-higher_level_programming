c3f0a794b0839efd3ea09a9073a0d73ab08e1e0f
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-07-11 07:55:42 +0100

    fix: Fix inherits from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,4 +15,4 @@ def inherits_from(obj, a_class):
         bool: True if the object is an instance of a class that inherited
         from the specified class, False otherwise.
     """
-    return isinstance(obj, type) and issubclass(type(obj), a_class) and obj.__class__ is not a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class
