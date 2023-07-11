6f5027c8b7baad4df5120949ead06a13438893f5
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-07-11 05:56:24 +0100

    feat: Implement add attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a function that adds attributes to objects."""
+
+
+def add_attribute(obj, attr_name, attr_value):
+    """
+    Adds a new attribute to an object if it's possible.
+
+    Args:
+        obj: The object to which the attribute is to be added.
+        attr_name (str): The name of the attribute.
+        attr_value: The value of the attribute to be added.
+
+    Raises:
+        TypeError: If the object can't have a new attribute.
+
+    Returns:
+        None
+    """
+    if not hasattr(obj, '__dict__') and not hasattr(type(obj), '__slots__'):
+        raise TypeError("can't add new attribute")
+
+    setattr(obj, attr_name, attr_value)
