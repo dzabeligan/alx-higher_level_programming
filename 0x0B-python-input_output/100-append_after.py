cf9a426ef3f8871bcd21a3cca11452709254d4fd
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-07-12 06:27:54 +0100

    feat: Update search and insert

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,11 +10,12 @@ def append_after(filename="", search_string="", new_string=""):
         search_string (str): The string to search for within the file.
         new_string (str): The string to insert.
     """
-    text = ""
-    with open(filename) as r:
-        for line in r:
-            text += line
+    with open(filename, "r+") as file:
+        lines = file.readlines()
+        file.seek(0)  # Move the file pointer to the beginning
+        for line in lines:
+            file.write(line)
             if search_string in line:
-                text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+                file.write(new_string)
+        # Remove any remaining content after the modified lines
+        file.truncate()
