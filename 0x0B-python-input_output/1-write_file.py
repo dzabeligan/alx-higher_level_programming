210352e48137ccd09d8c68f1009058f311f4fb6b
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-07-12 06:10:28 +0100

    fix: Placate lint

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,16 +4,16 @@
 
 def write_file(filename="", text=""):
     """
-    Writes the provided text to a file in UTF-8 encoding and returns the number
-    of characters written.
+    Writes the provided text to a file in UTF-8 encoding and returns the
+    number of characters written.
 
     Args:
         filename (str): The name of the file to write to.
         text (str): The content to be written to the file.
 
     Returns:
-        int: The number of characters written to the file. Returns 0 if an error
-        occurs.
+        int: The number of characters written to the file. Returns 0 if an
+        error occurs.
 
     Raises:
         None
