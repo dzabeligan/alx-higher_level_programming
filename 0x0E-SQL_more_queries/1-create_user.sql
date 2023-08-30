28df8d7e420a864c0ffa02b17b1766e304805cb1
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-08-30 08:51:37 +0100

    feat: Update SQL more queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,8 @@
 -- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,15 @@
 -- Lists all shows from hbtn_0d_tvshows_rate by their rating.
 -- Records are ordered by descending rating.
-SELECT `title`, SUM(`rate`) AS `rating`
-  FROM `tv_shows` AS t
-       INNER JOIN `tv_show_ratings` AS r
-       ON t.`id` = r.`show_id`
- GROUP BY `title`
- ORDER BY `rating` DESC;
+SELECT
+    g.name AS name,
+    SUM(r.rate) AS rating
+FROM
+    tv_show_genres AS sg
+JOIN
+    tv_genres AS g ON sg.genre_id = g.id
+JOIN
+    tv_show_ratings AS r ON sg.show_id = r.show_id
+GROUP BY
+    g.name
+ORDER BY
+    rating DESC, name;
