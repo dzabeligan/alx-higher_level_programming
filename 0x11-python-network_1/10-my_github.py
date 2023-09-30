b644f2c277a267adbe58ce6072e4cdf6401b1239
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-09-30 05:55:59 +0100

    feat: Implement My GitHub

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Uses the GitHub API to display a GitHub ID based on given credentials.
+
+Usage: ./10-my_github.py <GitHub username> <GitHub password>
+  - Uses Basic Authentication to access the ID.
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+r = requests.get("https://api.github.com/user", auth=auth, timeout=60)
+print(r.json().get("id"))
