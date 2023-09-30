fdf533cdce482d93d207d7f47d758d6dac13aa7b
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-09-30 05:14:39 +0100

    feat: Implement response header value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Displays the value of `X-Request-Id` header variable in the response.
+
+Usage: ./1-hbtn_header.py <URL>
+"""
+
+import sys
+import urllib.request
+
+request = urllib.request.Request(sys.argv[1])
+with urllib.request.urlopen(request) as response:
+    print(dict(response.headers).get("X-Request-Id"))
