37103190c974296aa76ffb69476f539fa6c2a52f
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-09-30 05:09:12 +0100

    feat: Implement What's my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status"""
+
+import urllib.request
+
+request = urllib.request.Request("https://intranet.hbtn.io/status")
+with urllib.request.urlopen(request) as response:
+    body = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
