6a699cf9465eb1a3d47e6bd6e61aa17909aa0f02
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-09-30 05:43:19 +0100

    feat: Implement error code again

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
+import sys
+import requests
+
+res = requests.get(sys.argv[1], timeout=60)
+if res.status_code >= 400:
+    print(f"Error code: {res.status_code}")
+else:
+    print(res.text)
