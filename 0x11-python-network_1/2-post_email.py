4b3e0a773bb99d60017970f4b0a9cc5ea0ae6b2f
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-09-30 05:20:12 +0100

    feat: Implement Post an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
+import sys
+from urllib import parse, request
+
+
+req = request.Request(sys.argv[1], parse.urlencode({
+    "email": sys.argv[2]}).encode("ascii"))
+with request.urlopen(req) as response:
+    print(response.read().decode("utf-8"))
